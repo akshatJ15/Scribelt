251f8bad9b52ddf1bd38a53b7846f9583f027675
Aakshat Jindal <jindalaj15akshat2002@gmail.com>
2025-01-18 17:25:12 +0530

    Add Tiptap task item and task list extensions with styling

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,17 +1,20 @@
 "use client";
 
 import StarterKit from "@tiptap/starter-kit";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
 import { useEditor, EditorContent } from "@tiptap/react";
 
 export const Editor = () => {
   const editor = useEditor({
     editorProps: {
       attributes: {
-        style:"padding-left:56px; padding-right:56px;",
-        class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
+        style: "padding-left:56px; padding-right:56px;",
+        class:
+          "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
     },
-    extensions: [StarterKit],
+    extensions: [StarterKit, TaskItem.configure({ nested: true }), TaskList],
     content: `<p>Hello World! 🌍️👋</p>`,
   });
   return (
