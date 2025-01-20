d29222885e38dbc3de6585946cc10281b89ae974
Aakshat Jindal <jindalaj15akshat2002@gmail.com>
2025-01-21 01:40:44 +0530

    Add font family and text style extensions to the editor

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -12,6 +12,8 @@ import Image from "@tiptap/extension-image";
 import ImageResize from "tiptap-extension-resize-image";
 import { useEditorStore } from "@/store/use-editor-store";
 import Underline from "@tiptap/extension-underline";
+import FontFamily from "@tiptap/extension-font-family";
+import TextStyle from "@tiptap/extension-text-style";
 
 export const Editor = () => {
   const { setEditor } = useEditorStore();
@@ -54,6 +56,8 @@ export const Editor = () => {
       StarterKit,
       TaskItem.configure({ nested: true }),
       TaskList,
+      FontFamily,
+      TextStyle,
       Table,
       TableCell,
       TableHeader,
--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -11,12 +11,141 @@ import {
   MessageSquarePlusIcon,
   ListTodoIcon,
   RemoveFormattingIcon,
-
+  ChevronDownIcon,
 } from "lucide-react";
 import React from "react";
 import { cn } from "@/lib/utils";
 import { useEditorStore } from "@/store/use-editor-store";
 import { Separator } from "@/components/ui/separator";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { type Level } from "@tiptap/extension-heading";
+
+const FontFamilyButton = () => {
+  const { editor } = useEditorStore();
+  const fonts = [
+    { label: "Arial", value: "Arial" },
+    { label: "Courier New", value: "Courier New" },
+    { label: "Times New Roman", value: "Times New Roman" },
+    { label: "Georgia", value: "Georgia" },
+    { label: "Verdana", value: "Verdana" },
+    { label: "Trebuchet MS", value: "Trebuchet MS" },
+    { label: "Tahoma", value: "Tahoma" },
+    { label: "Comic Sans MS", value: "Comic Sans MS" },
+    { label: "Impact", value: "Impact" },
+    { label: "Lucida Console", value: "Lucida Console" },
+  ];
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button
+          className={cn(
+            "h-7 w-[120px] shrink-0 flex items-center justify-between rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+          )}
+        >
+          <span className="truncate">
+            {editor?.getAttributes("textStyle")?.fontFamily || "Arial"}
+          </span>
+          <ChevronDownIcon className="size-4 ml-2 shrink-0" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+        {fonts.map((font) => {
+          return (
+            <button
+              className={
+                (cn(
+                  "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80"
+                ),
+                editor?.getAttributes("textStyle")?.fontFamily === font.value
+                  ? "bg-neutral-200/80"
+                  : "")
+              }
+              style={{ fontFamily: font.value }}
+              key={font.label}
+              onClick={() => {
+                editor?.chain().focus().setFontFamily(font.value).run();
+              }}
+            >
+              <span className="text-sm">{font.label}</span>
+            </button>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+const HeadingLevelButton = () => {
+  const { editor } = useEditorStore();
+  const headings = [
+    { label: "Normal text", value: 0, fontSize: "16px" },
+    { label: "Heading 1", value: 1, fontSize: "32px" },
+    { label: "Heading 2", value: 2, fontSize: "24px" },
+    { label: "Heading 3", value: 3, fontSize: "20px" },
+    { label: "Heading 4", value: 4, fontSize: "18px" },
+    { label: "Heading 5", value: 5, fontSize: "16px" },
+  ];
+
+  const getCurrentHeading = () => {
+    for (let level = 1; level <= 5; level++) {
+      if (editor?.isActive("heading", { level })) {
+        return `Heading ${level}`;
+      }
+    }
+    return "Normal text";
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button
+          className={cn(
+            "h-7 min-w-7 shrink-0 flex items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+          )}
+        >
+          <span className="truncate">{getCurrentHeading()}</span>
+          <ChevronDownIcon className="size-4 ml-2 shrink-0" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+        {headings.map(({ label, value, fontSize }) => {
+          return (
+            <button
+              key={value}
+              style={{ fontSize }}
+              className={cn(
+                "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+
+                (value === 0 && !editor?.isActive("heading")) ||
+                  editor?.isActive("heading", { level: value })
+                  ? "bg-neutral-200/80"
+                  : ""
+              )}
+              onClick={() => {
+                if (value == 0) {
+                  editor?.chain().focus().setParagraph().run();
+                } else {
+                  editor
+                    ?.chain()
+                    .focus()
+                    .setHeading({ level: value as Level })
+                    .run();
+                }
+              }}
+            >
+              <span className="text-sm">{label}</span>
+            </button>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
 interface ToolbarButtonProps {
   onClick?: () => void;
@@ -118,42 +247,49 @@ const toolbar = () => {
       },
     ],
     [
-        {
-            label: "Comment",
-            icon: MessageSquarePlusIcon,
-            onClick: () => {
-                console.log("comment");
-            },
-            isActive: editor?.isActive("comment"),
+      {
+        label: "Comment",
+        icon: MessageSquarePlusIcon,
+        onClick: () => {
+          console.log("comment");
         },
-        {
-            label: "List Todo",
-            icon: ListTodoIcon,
-            onClick: () => {
-                console.log("comment");
-                editor?.chain().focus().toggleTaskList().run();
-            },
-            isActive: editor?.isActive("taskList"),
+        isActive: editor?.isActive("comment"),
+      },
+      {
+        label: "List Todo",
+        icon: ListTodoIcon,
+        onClick: () => {
+          console.log("comment");
+          editor?.chain().focus().toggleTaskList().run();
         },
-        {
-            label: "Remove Formatting",
-            icon: RemoveFormattingIcon,
-            onClick: () => {
-                console.log("comment");
-                editor?.chain().focus().unsetAllMarks().run();
-            },
+        isActive: editor?.isActive("taskList"),
+      },
+      {
+        label: "Remove Formatting",
+        icon: RemoveFormattingIcon,
+        onClick: () => {
+          console.log("comment");
+          editor?.chain().focus().unsetAllMarks().run();
         },
-    ]
+      },
+    ],
   ];
   return (
-    <div className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto">
+    <div
+      className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto"
+      suppressHydrationWarning
+    >
       {sections[0].map((item) => {
         return <ToolbarButton {...item} key={item.label} />;
       })}
       <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      <FontFamilyButton />
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
       {sections[1].map((item) => {
         return <ToolbarButton {...item} key={item.label} />;
       })}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      <HeadingLevelButton />
       <Separator orientation="vertical" className="h-6 bg-neutral-300" />
       {sections[2].map((item) => {
         return <ToolbarButton {...item} key={item.label} />;
